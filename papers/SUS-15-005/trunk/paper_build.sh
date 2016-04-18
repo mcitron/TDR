6299eb4f0c5b5e2dc0537c848b1d997b0c78112d
bainbrid <robert.bainbridge@cern.ch>
2016-04-18 15:41:27 +0200

    triv

--- papers/SUS-15-005/trunk/paper_build.sh
+++ papers/SUS-15-005/trunk/paper_build.sh
@@ -1,3 +1,5 @@
 eval `../../tdr runtime -sh` 
-tdr --style=paper -aps=- b SUS-15-005
+tdr --style=paper -aps=- b SUS-15-005 # no numbered sections, easily read
+#tdr --style=paper -plb=- b SUS-15-005 # numbered sections, easily read, reference formatting screwed 
+#tdr --style=paper -epjc=- b SUS-15-005 # numbered sections, but dense text
 cp ../../tmp/SUS-15-005_temp.pdf ~/www/
