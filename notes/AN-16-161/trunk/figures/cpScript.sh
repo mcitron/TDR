c396d8427844555bdcb81ee64c7fff4ad1cf09de
Stefano Casasso <stefano.casasso@cern.ch>
2016-05-24 10:44:18 +0100

    first commit for new AN

---
+++ notes/AN-16-161/trunk/figures/cpScript.sh
@@ -0,0 +1,3 @@
+input13TeVExp=/Users/matthewcitron/Work/shapeStuff/test13TeVExpectedLumi3/scale_ht_variable_mht/
+cp ${input13TeVExp}/Ttw/fitOut/Linear2DShiftMean/frenchFlagErrComplete_Linear2DShiftMean_p1_Ttw.pdf $output3
+cp ${input13TeVExp}/Zinv/fitOut/Linear2DShiftMean/frenchFlagErrComplete_Linear2DShiftMean_p1_Zinv.pdf $output3
