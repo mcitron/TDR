6a2c1370236313a527855d01ac40a3099eefb51f
bainbrid <robert.bainbridge@cern.ch>
2017-05-17 12:21:20 +0200

    added latex

--- notes/AN-17-122/trunk/binning/binning.py
+++ notes/AN-17-122/trunk/binning/binning.py
@@ -1,7 +1,15 @@
 import pickle 
 from collections import OrderedDict as odict
 
-file = open("MhtBinning_final_v2.pkl","rb")
+def rreplace(s, old, new, occurrence):
+    li = s.rsplit(old, occurrence)
+    return new.join(li)
+
+# SR or CR? 
+region_str = "SR"
+region = {"CR":0,"SR":1}[region_str]
+
+file = open(["MhtBinning_final_CR.pkl","MhtBinning_final_SR.pkl"][region],"rb")
 input = pickle.load(file)
 
 # format of input dict:
@@ -10,8 +18,24 @@ input = pickle.load(file)
 
 njet_bins = ["eq1j","ge2a","eq2j","eq3j","eq4j","eq5j","ge6j"]
 nb_bins = ["eq0b","eq1b","eq2b","eq3b","ge4b"]
-ht_bins = [[200,250,300,350,400,500,600,750,900,1050,1200],[200,400,600,900,1200]][0]
+ht_bins = [[200,250,300,350,400,500,600,750,900,1050,1200],[200,400,600,900,1200]][region]
 mht_bins = [200,400,600,900]
+
+njet_string = {"eq1j":" = 1",
+               "ge2a":" \\geq 2 \\textrm{(asymmetric topology)}",
+               "eq2j":" = 2",
+               "eq3j":" = 3",
+               "eq4j":" = 4",
+               "eq5j":" = 5",
+               "ge6j":" \\geq 6",
+               }
+
+nb_string = {"eq0b":"0",
+             "eq1b":"1",
+             "eq2b":"2",
+             "eq3b":"3",
+             "ge4b":"$\\geq 4$",
+             }
 
 # print input dict
 for key,val in input.items() :
@@ -82,30 +106,58 @@ else :
 
     for njet in njet_bins :
         print 
-        print njet
-        print "     &"," & ".join([ "{:5.0f}".format(ht) for ht in ht_bins ]),"&"
+        if njet == "eq1j" :
+            caption  = "Summary of the highest value of \\nb used to categorise events "
+            caption += "as a function of \\scalht [GeV] and \\HTmiss [GeV] for events "
+            caption += "that satisfy $\\njet{:s}$. ".format(njet_string[njet])
+            caption += "An identical \\HTmiss binning is used for each \\nb category, "
+            caption += "from $\\nb = 0$ up to the \\nb value indicated. "
+            if region == 0 :
+                caption += "The \\scalht binning scheme corresponds to that used by "
+                caption += "the control regions. (Event yields and predictions are "
+                caption += "subsequently aggregated according to the binning scheme "
+                caption += "for the signal region.) "
+            else :
+                caption += "The \\scalht binning scheme corresponds to the "
+                caption += "aggregated scheme used by the signal region. "
+            caption += "The first entry per columnn signifies the lower bound of "
+            caption += "the first \\HTmiss bin. The final entry per columnn signifies "
+            caption += "the lower bound of the final open \\HTmiss bin. " 
+            caption += "Gaps between entries within a column signify merged \\HTmiss bins. "
+            caption += "An empty column signifies that the (\\njet, \\scalht) "
+            caption += "category is not used in the analysis. "
+        else :
+            caption  = "As for Table~\\ref{tab:"+"{:s}_binning_eq1j".format(region_str)+"} with events that satisfy "
+            caption += "$\\njet{:s}$. ".format(njet_string[njet])
+        string = "\\begin{table}[!h]\n"
+        string += "  \\topcaption{"+caption+"}\n"
+        string += "  \\label{tab:"+"{:s}_binning_{:s}".format(region_str,njet)+"}\n"
+        string += "  \\centering\n"
+        string += "  \\begin{tabular}{l"+"c"*len(ht_bins)+"}\n"
+        string += "  \\hline\n"
+        string += "  \\scalht [GeV] & \\multicolumn{"+str(len(ht_bins))+"}{c}{\\HTmiss [GeV]} \\\\ \n"
+        string += "  \\cline{2-"+str(len(ht_bins)+1)+"}"
+        print string
+        print "      &"," & ".join([ "{:8.0f}".format(ht) for ht in ht_bins ]),"\\\\"
+        print "  \\hline"
         for mht in mht_bins :
-            print "{:4.0f} & ".format(mht),
+            temp = " {:4.0f} & ".format(mht)
             for ht in ht_bins :
                 # select subset of dict based on keys wtih correct njet and ht
-                ht_dict = odict([ (key[1],val) for key,val in input.items() if njet in key[0] and ht == key[2][0] ]) 
+                ht_dict = odict([ (key,val) for key,val in input.items() if njet in key[0] and ht == key[2][0] ]) 
                 # print ht_dict
                 nb_val = ""
-                for nb,val in ht_dict.items() : # reverse nb direction 
-                    if len(val) and mht in val : nb_val = nb
-                print "{:4s} & ".format(nb_val),
-            print 
+                for nb,val in ht_dict.items() : 
+                    #print nb,val
+                    if len(val) and mht in val : nb_val = nb_string[nb[1]]
+                temp += "{:8s} & ".format(nb_val)
+            temp = rreplace(temp,"&","\\\\",1)
+            print temp
+        print "  \\end{tabular}"
+        print "\\end{table}"
 
+#for key,val in odict([ (key,val) for key,val in input.items() if "ge6j" in key[0] ]).items() : print key,val
+#print odict([ (key[1],val) for key,val in input.items() if "ge6j" in key[0] and 1200 == key[2][0] ]) 
 
-
-
-# format original dict to create latex for full tables 
-
-#        caption = "test"
-##        caption = "Summary of the lower bound of the final \HTmiss bin [GeV] as a function of \njet and \scalht (signal region, aggregated scheme). The lower bound of the first \scalht bin is summarised in Table~\ref{tab:sr-ht-binning}. The dashes (--) signify (\njet, \nb) bins that are not possible or used." 
-#
-#        string = "\\begin{table}[!h]\n"
-#        string += "  \\topcaption{"+caption+"}\n"
-#        string += "  \\label{tab:"+"cr_binning_{:s}_{:s}".format(njet,nb)+"}\n"
 
 
