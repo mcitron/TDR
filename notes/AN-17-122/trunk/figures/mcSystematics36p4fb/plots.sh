61ab0ae17a0bce9aa9f82dda978c55cd07c417d8
bainbrid <robert.bainbridge@cern.ch>
2017-05-30 12:21:14 +0200

    removed unnecessary files

--- notes/AN-17-122/trunk/figures/mcSystematics36p4fb/plots.sh
+++ notes/AN-17-122/trunk/figures/mcSystematics36p4fb/plots.sh
@@ -1,6 +1,7 @@
-DIR=http://www.hep.ph.ic.ac.uk/~sdb15/Analysis/RA1/201705_May/18_TransferFactors/
+#DIR=http://www.hep.ph.ic.ac.uk/~sdb15/Analysis/RA1/201705_May/18_TransferFactors/
+DIR=http://www.hep.ph.ic.ac.uk/~sdb15/Analysis/RA1/201705_May/30_TransferFactors/
 rm ./plots.tar.gz
-wgetr ${DIR}/plots.tar.gz 
+wgetr ${DIR}/plots/plots.tar.gz 
 tar zxvf plots.tar.gz
 cd ./plots/
 rm tf*.png
@@ -10,3 +11,4 @@ rm tf*_gj_*.pdf
 rm tf*_mu_Zinv_*.pdf
 rm tf*_mu_Ewk_*.pdf
 rm tf*_mumu_Ttw_*.pdf
+rm tf*_mumu_Ewk_*.pdf
