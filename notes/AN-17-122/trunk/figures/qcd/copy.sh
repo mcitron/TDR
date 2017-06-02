b91ad9727e0973422f9cc6ed04398fbde63c15cc
bainbrid <robert.bainbridge@cern.ch>
2017-06-02 13:09:10 +0200

    moved files, new copy script

---
+++ notes/AN-17-122/trunk/figures/qcd/copy.sh
@@ -0,0 +1,36 @@
+DIR="http://www.hep.ph.ic.ac.uk/~sdb15/Analysis/RA1/201706_Jun/01_Fit_QcdEstimation/plots/"
+
+cd sb_mhtmet/
+wgetr ${DIR}/Estimation/QcdSb/obsYields_QcdSb.pdf	   
+wgetr ${DIR}/Estimation/QcdSb/postFitEwkYields_QcdSb.pdf 
+wgetr ${DIR}/Estimation/QcdSb/postFitQcdYields_QcdSb.pdf 
+wgetr ${DIR}/Estimation/QcdSb/qcdSbEwk_MC.pdf		   
+wgetr ${DIR}/Estimation/QcdSb/rateParsQcd_QcdSb.pdf
+cd ../
+
+cd sb_bdphi/
+wgetr ${DIR}/Estimation/BDPhiSb/obsYields_QcdSb.pdf	   
+wgetr ${DIR}/Estimation/BDPhiSb/postFitEwkYields_QcdSb.pdf 
+wgetr ${DIR}/Estimation/BDPhiSb/postFitQcdYields_QcdSb.pdf 
+wgetr ${DIR}/Estimation/BDPhiSb/qcdSbEwk_MC.pdf		   
+wgetr ${DIR}/Estimation/BDPhiSb/rateParsQcd_QcdSb.pdf
+cd ../
+
+cd sb_double/
+wgetr ${DIR}/Estimation/DoubleSb/obsYields_QcdSb.pdf	   
+wgetr ${DIR}/Estimation/DoubleSb/postFitEwkYields_QcdSb.pdf 
+wgetr ${DIR}/Estimation/DoubleSb/postFitQcdYields_QcdSb.pdf 
+wgetr ${DIR}/Estimation/DoubleSb/qcdSbEwk_MC.pdf		   
+wgetr ${DIR}/Estimation/DoubleSb/rateParsQcd_QcdSb.pdf
+cd ../
+
+cd sb_mhtmet/
+wgetr ${DIR}/Estimation/QcdSb/predictedQcdYields_Signal.pdf
+wgetr ${DIR}/Estimation/QcdSb/predictedEwkYields_Signal.pdf
+wgetr ${DIR}/Estimation/QcdSb/predictedQcdDivEwk_Signal.pdf
+cd ../
+
+cd val_njet_ht/
+wgetr ${DIR}/Validation/Combined/rateParRatioMhtMetDivDouble1d.pdf
+wgetr ${DIR}/Validation/Combined/rateParRatioBDPhiDivDoubleSb1d.pdf
+cd ../
