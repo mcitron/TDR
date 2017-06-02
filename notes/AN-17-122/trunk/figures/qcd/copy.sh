8834f183287269b2b9b5919e078e5ec35e486019
bainbrid <robert.bainbridge@cern.ch>
2017-06-02 13:34:39 +0200

    tweaked script

--- notes/AN-17-122/trunk/figures/qcd/copy.sh
+++ notes/AN-17-122/trunk/figures/qcd/copy.sh
@@ -1,36 +1,39 @@
+alias wget='wget -e robots=off --no-parent -nH -nd -N --no-use-server-timestamps -c -r -l1 -R index.html*'
 DIR="http://www.hep.ph.ic.ac.uk/~sdb15/Analysis/RA1/201706_Jun/01_Fit_QcdEstimation/plots/"
 
 cd sb_mhtmet/
-wgetr ${DIR}/Estimation/QcdSb/obsYields_QcdSb.pdf	   
-wgetr ${DIR}/Estimation/QcdSb/postFitEwkYields_QcdSb.pdf 
-wgetr ${DIR}/Estimation/QcdSb/postFitQcdYields_QcdSb.pdf 
-wgetr ${DIR}/Estimation/QcdSb/qcdSbEwk_MC.pdf		   
-wgetr ${DIR}/Estimation/QcdSb/rateParsQcd_QcdSb.pdf
+wget ${DIR}/Estimation/QcdSb/obsYields_QcdSb.pdf	   
+wget ${DIR}/Estimation/QcdSb/postFitEwkYields_QcdSb.pdf 
+wget ${DIR}/Estimation/QcdSb/postFitQcdYields_QcdSb.pdf 
+wget ${DIR}/Estimation/QcdSb/qcdSbEwk_MC.pdf		   
+wget ${DIR}/Estimation/QcdSb/rateParsQcd_QcdSb.pdf
 cd ../
 
 cd sb_bdphi/
-wgetr ${DIR}/Estimation/BDPhiSb/obsYields_QcdSb.pdf	   
-wgetr ${DIR}/Estimation/BDPhiSb/postFitEwkYields_QcdSb.pdf 
-wgetr ${DIR}/Estimation/BDPhiSb/postFitQcdYields_QcdSb.pdf 
-wgetr ${DIR}/Estimation/BDPhiSb/qcdSbEwk_MC.pdf		   
-wgetr ${DIR}/Estimation/BDPhiSb/rateParsQcd_QcdSb.pdf
+wget ${DIR}/Estimation/BDPhiSb/obsYields_QcdSb.pdf	   
+wget ${DIR}/Estimation/BDPhiSb/postFitEwkYields_QcdSb.pdf 
+wget ${DIR}/Estimation/BDPhiSb/postFitQcdYields_QcdSb.pdf 
+wget ${DIR}/Estimation/BDPhiSb/qcdSbEwk_MC.pdf		   
+wget ${DIR}/Estimation/BDPhiSb/rateParsQcd_QcdSb.pdf
 cd ../
 
 cd sb_double/
-wgetr ${DIR}/Estimation/DoubleSb/obsYields_QcdSb.pdf	   
-wgetr ${DIR}/Estimation/DoubleSb/postFitEwkYields_QcdSb.pdf 
-wgetr ${DIR}/Estimation/DoubleSb/postFitQcdYields_QcdSb.pdf 
-wgetr ${DIR}/Estimation/DoubleSb/qcdSbEwk_MC.pdf		   
-wgetr ${DIR}/Estimation/DoubleSb/rateParsQcd_QcdSb.pdf
+wget ${DIR}/Estimation/DoubleSb/obsYields_QcdSb.pdf	   
+wget ${DIR}/Estimation/DoubleSb/postFitEwkYields_QcdSb.pdf 
+wget ${DIR}/Estimation/DoubleSb/postFitQcdYields_QcdSb.pdf 
+wget ${DIR}/Estimation/DoubleSb/qcdSbEwk_MC.pdf		   
+wget ${DIR}/Estimation/DoubleSb/rateParsQcd_QcdSb.pdf
 cd ../
 
-cd sb_mhtmet/
-wgetr ${DIR}/Estimation/QcdSb/predictedQcdYields_Signal.pdf
-wgetr ${DIR}/Estimation/QcdSb/predictedEwkYields_Signal.pdf
-wgetr ${DIR}/Estimation/QcdSb/predictedQcdDivEwk_Signal.pdf
+cd pred/
+wget ${DIR}/Estimation/QcdSb/predictedQcdYields_Signal.pdf
+wget ${DIR}/Estimation/QcdSb/predictedEwkYields_Signal.pdf
+wget ${DIR}/Estimation/QcdSb/predictedQcdDivEwk_Signal.pdf
 cd ../
 
 cd val_njet_ht/
-wgetr ${DIR}/Validation/Combined/rateParRatioMhtMetDivDouble1d.pdf
-wgetr ${DIR}/Validation/Combined/rateParRatioBDPhiDivDoubleSb1d.pdf
+wget ${DIR}/Validation/Combined/rateParRatioMhtMetDivDouble1d.pdf
+wget ${DIR}/Validation/Combined/rateParRatioBDPhiDivDoubleSb1d.pdf
 cd ../
+
+unalias wget
