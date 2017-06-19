563cc35a52df5a64464429cca83bb39c26bf85b5
bainbrid <robert.bainbridge@cern.ch>
2017-06-19 17:30:43 +0200

    updated script

--- notes/AN-17-122/trunk/figures/results/copy.sh
+++ notes/AN-17-122/trunk/figures/results/copy.sh
@@ -1,60 +1,44 @@
-alias wget='wget -e robots=off --no-parent -nH --cut-dirs=4 -r -R index.html*,*.png,*.root,*.txt,*.csv,*.pkl'
-#alias wget='wget -e robots=off -nH -r -R index.html*'
-#alias wget='wget -e robots=off --no-parent -nH -c -r -R index.html*'
+alias wget='wget -e robots=off --no-parent -nH -r -R index.html*,*.png,*.root,*.txt,*.csv,*.pkl -nd'
+# --cut-dirs=5'
 
-MR="https://www.hep.ph.ic.ac.uk/~cl4010/RA1/Stats/Fit_wMHT_1in8_noQcdMono_toys200_Mountain/"
-NUIS_PRE="https://www.hep.ph.ic.ac.uk/~cl4010/RA1/Stats/Fit_wMHT_1in8_noQcdMono_toys1_nuisances-prefit/"
-NUIS_POST="https://www.hep.ph.ic.ac.uk/~cl4010/RA1/Stats/Fit_wMHT_1in8_noQcdMono_toys1_nuisances-postfit/"
-SHAPE_PRE="https://www.hep.ph.ic.ac.uk/~cl4010/RA1/Stats/Fit_wMHT_1in8_noQcdMono_toys200_Shapes-prefit/"
-SHAPE_POST="https://www.hep.ph.ic.ac.uk/~cl4010/RA1/Stats/Fit_wMHT_1in8_noQcdMono_toys200_Shapes-postfit/"
-SHAPE_CR="https://www.hep.ph.ic.ac.uk/~cl4010/RA1/Stats/Fit_wMHT_1in8_noQcdMono_toys200_Shapes-CRfit/"
+# 1 in 8
+#BASE="1in8/"
+#MR="https://www.hep.ph.ic.ac.uk/~cl4010/RA1/Stats/Fit_wMHT_1in8_noQcdMono_toys200_Mountain/"
+#NUIS_PRE="https://www.hep.ph.ic.ac.uk/~cl4010/RA1/Stats/Fit_wMHT_1in8_noQcdMono_toys1_nuisances-prefit/"
+#NUIS_POST="https://www.hep.ph.ic.ac.uk/~cl4010/RA1/Stats/Fit_wMHT_1in8_noQcdMono_toys1_nuisances-postfit/"
+#SHAPE_PRE="https://www.hep.ph.ic.ac.uk/~cl4010/RA1/Stats/Fit_wMHT_1in8_noQcdMono_toys200_Shapes-prefit/"
+#SHAPE_POST="https://www.hep.ph.ic.ac.uk/~cl4010/RA1/Stats/Fit_wMHT_1in8_noQcdMono_toys200_Shapes-postfit/"
+#SHAPE_CR="https://www.hep.ph.ic.ac.uk/~cl4010/RA1/Stats/Fit_wMHT_1in8_noQcdMono_toys200_Shapes-CRfit/"
 
-#cd mono/
-#wget ${MR}/summaryPlot_Monojet_prefit.pdf
-#wget ${MR}/summaryPlot_Monojet_fit_b.pdf
-#wget ${MR}/summaryPlot_Monojet_prefit_overlay_fit_b.pdf	
-#cd ../
-#
-#cd asym/
-#wget ${MR}/summaryPlot_Asymmetric_prefit.pdf
-#wget ${MR}/summaryPlot_Asymmetric_fit_b.pdf
-#wget ${MR}/summaryPlot_Asymmetric_prefit_overlay_fit_b.pdf	
-#cd ../
-#
-#cd symm/
-#wget ${MR}/summaryPlot_Symmetric_prefit.pdf
-#wget ${MR}/summaryPlot_Symmetric_fit_b.pdf
-#wget ${MR}/summaryPlot_Symmetric_prefit_overlay_fit_b.pdf	
-#cd ../
-#
-#cd all/
-#wget ${MR}/ratios_all_prefit.pdf
-#wget ${MR}/pulls_all_prefit.pdf
-#cd ../
-#
-#cd prefit/nuis/
-#wget ${NUIS_PRE}/
-#rm *.png
-#cd ../../
-#
-#cd postfit/nuis/
-#wget ${NUIS_POST}/
-#rm *.png
-#cd ../../
-#
-#cd prefit/shapes/
-#wget ${SHAPE_PRE}/
-#rm *.png
-#cd ../../
+# Unblinded 
+BASE="36invfb/"
+MR="http://www.hep.ph.ic.ac.uk/~klo2/RA1/BTagFormula/FullFit/Log/20170616/Unblinding36fb2017_v3_IncMht_formulaSyst/postFitNorm/"
+NUIS_PRE="http://www.hep.ph.ic.ac.uk/~klo2/RA1/BTagFormula/FullFit/Log/20170616/Unblinding36fb2017_v3_IncMht_formulaSyst/postFitNuisance/" #@@
+NUIS_POST="http://www.hep.ph.ic.ac.uk/~klo2/RA1/BTagFormula/FullFit/Log/20170616/Unblinding36fb2017_v3_IncMht_formulaSyst/postFitNuisance/"
+SHAPE_PRE="http://www.hep.ph.ic.ac.uk/~klo2/RA1/BTagFormula/FullFit/Log/20170616/Unblinding36fb2017_v3_formulaSyst/postCRFitShape/" #@@
+SHAPE_POST="http://www.hep.ph.ic.ac.uk/~klo2/RA1/BTagFormula/FullFit/Log/20170616/Unblinding36fb2017_v3_formulaSyst/postFitShape/"
+SHAPE_CR="http://www.hep.ph.ic.ac.uk/~klo2/RA1/BTagFormula/FullFit/Log/20170616/Unblinding36fb2017_v3_formulaSyst/postCRFitShape/"
 
-cd crfit/shapes/
-wget ${SHAPE_CR}/
-rm *.png
-cd ../../
+wget -P ${BASE}/mono/ ${MR}/summaryPlot_Monojet_prefit.pdf
+wget -P ${BASE}/mono/ ${MR}/summaryPlot_Monojet_fit_b.pdf
+wget -P ${BASE}/mono/ ${MR}/summaryPlot_Monojet_prefit_overlay_fit_b.pdf
 
-#cd postfit/shapes/
-#wget ${SHAPE_POST}/
-#rm *.png
-#cd ../../
+wget -P ${BASE}/asym/ ${MR}/summaryPlot_Asymmetric_prefit.pdf
+wget -P ${BASE}/asym/ ${MR}/summaryPlot_Asymmetric_fit_b.pdf
+wget -P ${BASE}/asym/ ${MR}/summaryPlot_Asymmetric_prefit_overlay_fit_b.pdf
+
+wget -P ${BASE}/symm/ ${MR}/summaryPlot_Symmetric_prefit.pdf
+wget -P ${BASE}/symm/ ${MR}/summaryPlot_Symmetric_fit_b.pdf
+wget -P ${BASE}/symm/ ${MR}/summaryPlot_Symmetric_prefit_overlay_fit_b.pdf
+
+wget -P ${BASE}/all/ ${MR}/ratios_all_prefit.pdf
+wget -P ${BASE}/all/ ${MR}/pulls_all_prefit.pdf
+
+wget -P ${BASE}/prefit/nuis/ ${NUIS_PRE}/
+wget -P ${BASE}/postfit/nuis/ ${NUIS_POST}/
+
+wget -P ${BASE}/prefit/shapes/ ${SHAPE_PRE}/
+wget -P ${BASE}/crfit/shapes/ ${SHAPE_CR}/
+wget -P ${BASE}/postfit/shapes/ ${SHAPE_POST}/
 
 unalias wget
