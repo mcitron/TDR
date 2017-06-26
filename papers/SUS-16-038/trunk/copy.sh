553a652e995af7aaa23cdad6ee023b6bd5766dab
bainbrid <robert.bainbridge@cern.ch>
2017-06-26 12:17:55 +0200

    triv

--- papers/SUS-16-038/trunk/copy.sh
+++ papers/SUS-16-038/trunk/copy.sh
@@ -2,6 +2,7 @@ cp ../../../../AlphaTDR2/papers/SUS-16-038/trunk/*.tex .
 cp ../../../../AlphaTDR2/papers/SUS-16-038/trunk/*.bib .
 cp ../../../../AlphaTDR2/papers/SUS-16-038/trunk/*.pdf .
 cp ../../../../AlphaTDR2/papers/SUS-16-038/trunk/*.sh .
+cp ../../../../AlphaTDR2/papers/SUS-16-038/trunk/*.txt .
 #cp ../../../../AlphaTDR2/papers/SUS-16-038/trunk/*.html .
 #cp ../../../../AlphaTDR2/papers/SUS-16-038/trunk/*.sh .
 #cp ../../../../AlphaTDR2/papers/SUS-16-038/trunk/RootFilesAndTarFiles/*.* ./RootFilesAndTarFiles/
