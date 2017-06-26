6e0bac7446b674c1e46025b1f61a29f16d66fb00
bainbrid <robert.bainbridge@cern.ch>
2017-06-26 17:12:34 +0200

    tweak

--- papers/SUS-14-006/trunk/copy.sh
+++ papers/SUS-14-006/trunk/copy.sh
@@ -2,7 +2,7 @@ cp ../../../../AlphaTDR2/papers/SUS-14-006/trunk/*.tex .
 cp ../../../../AlphaTDR2/papers/SUS-14-006/trunk/*.bib .
 cp ../../../../AlphaTDR2/papers/SUS-14-006/trunk/*.pdf .
 cp ../../../../AlphaTDR2/papers/SUS-14-006/trunk/*.html .
-cp ../../../../AlphaTDR2/papers/SUS-14-006/trunk/build.sh .
 cp ../../../../AlphaTDR2/papers/SUS-14-006/trunk/copy.sh .
-cp ../../../../AlphaTDR2/papers/SUS-14-006/trunk/aux.sh .
+cp ../../../../AlphaTDR2/papers/SUS-14-006/trunk/build.sh .
+cp ../../../../AlphaTDR2/papers/SUS-14-006/trunk/build_suppl.sh .
 cp ../../../../AlphaTDR2/papers/SUS-14-006/trunk/RootFilesAndTarFiles/*.* RootFilesAndTarFiles
